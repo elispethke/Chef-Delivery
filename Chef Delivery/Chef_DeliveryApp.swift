dd0de5ccbc2d58dc9c5342ce674654025e80ebc8
Elis Pethke <elispethke@gmail.com>
2025-01-02 13:44:48 +0100

    Start the Project

---
+++ Chef Delivery/Chef_DeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chef_DeliveryApp.swift
+//  Chef Delivery
+//
+//  Created by Elis Pethke on 02/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Chef_DeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
